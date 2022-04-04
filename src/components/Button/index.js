7e69f05024e6874d3c35a0236da181ecb2df27f0
ingasafranova <sfrnv@yahoo.co.uk>
2022-04-04 09:35:55 +0100

    cors fix

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -3,7 +3,7 @@ import React from "react";
 //import buttonImage from "./Button.png"
 
 
-export default function Button({ handleClick,buttonImage}) {
+export default function Button({ handleClick,buttonImage,text}) {
   return (
     <div id="button1">
       <button onClick={handleClick}><img src={buttonImage} alt="title"/>{text}</button>
